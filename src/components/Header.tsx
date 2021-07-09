a5f1bac8411dbdcc5d5c760395bcb8f4012fb421
totozunino <mathiaszunino@gmail.com>
2021-07-09 10:42:31 -0300

    feat: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImg from "../assets/meals.jpg";
+
+const Header: React.FC = () => (
+  <>
+    <header className={classes.header}>
+      <h1>Food App</h1>
+      <button type="button">Add to cart</button>
+    </header>
+    <div className={classes["main-image"]}>
+      <img src={mealsImg} alt="Our food menu" />
+    </div>
+  </>
+);
+
+export default Header;
