f08bd6fa22a893739243d89dd44e0661667ee2c7
Mathias Zunino <mathiaszunino@gmail.com>
2021-07-12 18:51:57 -0300

    feat(meals): meals summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 const App: React.FC = () => (
   <div className="App">
     <Header />
+    <MealsSummary />
   </div>
 );
 
